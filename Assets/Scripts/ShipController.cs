470d25ddd6f943ff3fb8b07bf8da94039c0c4a7d
CoderSquirrel <saneschio@gmail.com>
2016-05-28 23:25:50 -0400

    transferung from drive to git

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+	
+	}
+}
